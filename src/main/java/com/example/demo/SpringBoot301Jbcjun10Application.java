7c43a555ec08bd01337c78b8e6cb57891fd422f9
jlepler <jlepler@montgomerycollege.edu>
2019-07-16 11:12:07 -0400

    initial commit: configured h2-console

---
+++ src/main/java/com/example/demo/SpringBoot301Jbcjun10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot301Jbcjun10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot301Jbcjun10Application.class, args);
+	}
+
+}
